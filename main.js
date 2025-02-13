089fd32f4857e0fe17ac653dd4569e252374c32c
Zekken0518 <159396410+Zekken0518@users.noreply.github.com>
2025-02-13 23:57:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,21 +21,76 @@ document.addEventListener("DOMContentLoaded", () => {
 
     Happy Valentines Day, it's been awhile my Jophy,
     We've come a long way, but it feels like yesterday.
-    
-    From daisies white to roses red,
-    Each one unique, a work of art.
-    Your fragrance sweet, a gentle breeze,
-    Forever etched within my heart.
+    You remember this song? 
+    This song is probably a song you won't forget,
+    Can I have this dance?
 
-    Through rain or shine, you stand tall,
-    A symbol of life's persistence.
-    In nature's grand tapestry, you shine,
-    A testament to existence.
+    Still vividly remember the warmth of your hand,
+    At the center of the ballroom we stand.
+    The day I stretched my arm to ask your hand for a dance,
+    Was the day that our love took its entrance.
 
-    Bloom on, dear flowers, far and wide,
-    And paint the world with your hues.
-    For in your presence, we find solace,
-    And the beauty of life renews.
+    Roses are red, violets are blue,
+    Would it be cringe of me to say that I Love You.
+    Philosophers tell meaningful stories they say,
+    But encyclopedias would be jealous of the words your eyes convey.
+
+    Your pure and innocent smile, 
+    Your laughter that echoes for miles.
+    The way I look at you,
+    Feels like i'm looking at a view.
+
+    Even through misunderstandings,
+    We've been through a lot of things.
+    We've been through storms and rains,
+    But through God our love remains.
+
+    I'm not good with words, but I hope you understand,
+    That my love for you is more than just a stand.
+    I love you more than words can say,
+    And I'll Love you more each passing day.
+
+    We've gone far, but this is just the beggining,
+    We will be going through a lot more, but we will be winning.
+    You stayed with me, even when I'm at my lowest,
+    Then you deserve me at my best.
+
+    We are going to argue more in the future,
+    But that doesn't mean our Love will rupture.
+    Argument is just a process of building a strong relationship,
+    It is not by any means a sign of a sinking ship.
+
+    I'm far away from you, but you keep me in your heart,
+    You keep me in your prayers, even when we're apart.
+    How could I not Love you, when you Love me this much,
+    I am grateful to God, for giving me you as such.
+
+    I'm sorry for the times I made you cry,
+    Because time is not on my side.
+    I'm sorry for the times I made you sad,
+    But I'm trying my best to be the best you've ever had.
+
+    I will repay the times when I were'nt there when you needed me most,
+    I will be there for you, even if I become a shade of a ghost.
+    Even if the world turns its back on you,
+    I will be there, standing beside you.
+
+    Let's continue on our studies,
+    Let's continue on our journey.
+    Let's continue to grow together,
+    Let's continue to be better.
+
+    When the comes and have what we've been dreaming,
+    It will be the time when we'll be smiling.
+    We'll be looking back at the times we've been through,
+    And we'll be thankful that we chose the love that we knew.
+
+    So can I have this dance?
+    Can I hold your hand?
+    Can I be the one you'll always stand?
+
+    I love you, Jophy,
+    Happy Valentines Day.
   `;
 
   scrollingTextElement.innerText = text;
